7b1c42fffe22db938f6ac5533ea6b412d1e973e1
krishna k <kittu.1214@gmail.com>
2021-02-12 19:23:02 -0500

    KK: registration.component changes

--- src/app/food-schedular/components/user-account/registration/registration.component.ts
+++ src/app/food-schedular/components/user-account/registration/registration.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-registration',
@@ -6,10 +8,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./registration.component.scss']
 })
 export class RegistrationComponent implements OnInit {
+  hide = true;
+  email = new FormControl('', [Validators.required, Validators.email]);
 
-  constructor() { }
+  constructor(private router: Router) { }
+  getErrorMessage() {
+    if (this.email.hasError('required')) {
+      return 'You must enter a value';
+    }
 
+    return this.email.hasError('email') ? 'Not a valid email' : '';
+  }
   ngOnInit(): void {
   }
-
+  redirectToSignIn(): void {
+    this.router.navigate(['food-schedular/useraccount/signin']);
+  }
 }
